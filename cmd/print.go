80819b2acdc02f052476e01a6822013d1e57632e
Armando Solheiro <armando.solheiro@jackexperts.com>
2025-03-15 09:34:44 -0300

    feat: improve tables in print-hc

--- cmd/print.go
+++ cmd/print.go
@@ -12,10 +12,16 @@ import (
 	"github.com/asolheiro/gita-healthcheck/internal/metrics"
 	"github.com/asolheiro/gita-healthcheck/internal/problem"
 	"github.com/asolheiro/gita-healthcheck/internal/security"
+	terminalutils "github.com/asolheiro/gita-healthcheck/internal/terminal-utils"
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/spf13/cobra"
 )
 
 //go:generate go run main.go
+
+func init() {
+	printHcCmd.Flags().StringVarP(&orgFilter, "org", "o", "", "Filter report to specific organization name")
+}
 
 var printHcCmd = &cobra.Command{
 	Use:   "print-hc",
@@ -29,6 +35,9 @@ It gives the user a complete markdown report of what he can see in the plataform
 in the cost of a few lines in CLI
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if orgFilter == "" {
+			log.Fatalln("Please, insert a '-o/--org'")
+		}
 		authResponse, err := auth.Authentication()
 		if err != nil {
 			log.Fatalln(err)
@@ -37,68 +46,93 @@ in the cost of a few lines in CLI
 		if err != nil {
 			log.Fatal(err)
 		}
+		
+		_, org := getOrg(userCount.Msg)
+		
+		var rows []table.Row
+		for _, cluster := range org.Clusters {
+			res := getInfoWithGoRoutine(*authResponse, cluster.ClusterID)
 
-		fmt.Printf("\n%d organizations found:\n", len(userCount.Msg))
-		for i, msg := range userCount.Msg {
-			fmt.Printf("\n  %v. %s (%s)\n", i + 1, msg.Organization.Name, msg.Organization.ID)
-			for j, cluster := range msg.Clusters {
-				fmt.Printf("   %d.%d. %s (%s)\n", i+1, j+1, cluster.Name, cluster.ClusterID)
-				
-				cm, _ := metrics.GetClusterMetrics(authResponse.AccessToken, cluster.ClusterID)
-				fmt.Println("    Usage:")
-				fmt.Printf("    - CPU: %.2f%%\n", cm.CPUUsePercentage)
-				fmt.Printf("    - Mem: %.2f%%\n", cm.MemoryUsePercentage,)
-				fmt.Printf("    - Pods: %d/%d\n", cm.TotalPods,cm.TotalPodCapacity)
-				
-				alerts, _ := alerts.GetAlerts(authResponse.AccessToken, cluster.ClusterID)
-				fmt.Println("\n    Alerts: ", len(alerts))
-				if len(alerts) > 0 {
-					for _, alert := range alerts {
-						fmt.Printf("      - %s.%s - %s\n", alert.Kind, alert.Namespace, alert.Msg)
-					}
-				}
-				incidents, err := incidents.GetIncidents(authResponse.AccessToken, cluster.ClusterID)
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println("\n    Incidents: ", incidents.Total)
-				if incidents.Total > 0 {
-					var (loki, monitoring, cattleSystem, gita, kubeSystem, certManager, others int)
-					for _, incident := range incidents.Msg {
-						switch incident.Namespace {
-						case "loki":
-							loki += 1
-						case "monitoring":
-							monitoring += 1
-						case "cattle-monitoring-system":
-							monitoring += 1
-						case "cattle-system":
-							cattleSystem += 1
-						case "gita":
-							gita+=1
-						case "kube-system":
-							kubeSystem += 1
-						case "cert-manager":
-							certManager += 1
-						default:
-							others += 1
-						}
-					}
-					fmt.Println("      - loki: ", loki)
-					fmt.Println("      - monitoring: ", monitoring)
-					fmt.Println("      - cattle-system: ", cattleSystem)
-					fmt.Println("      - gita: ", gita)
-					fmt.Println("      - kube-system: ", kubeSystem)
-					fmt.Println("      - cert-manager: ", certManager)
-					fmt.Println("      - others: ", others)
-				}
-				
-				securities, _ := security.GetSecurity(authResponse.AccessToken, cluster.ClusterID)
-				fmt.Println("\n    Security alerts: ", securities.Total)
-
-				problems, _ := problem.GetProblems(authResponse.AccessToken, cluster.ClusterID)
-				fmt.Println("\n    Problem alerts: ", problems.Total)
-			}
+			rows = append(rows, []string{
+				cluster.Name,
+				cluster.ClusterID,
+				res.usageCPU,
+				res.usageRAM,
+				res.usagePod,
+				res.alertsNum,
+				res.incidentsNum,
+				res.problemsNum,
+				res.securitiesNum,
+			})
 		}
+
+		fmt.Printf("\n %s (%s)\n", org.Organization.Name, org.Organization.ID)
+		terminalutils.RunTables(rows)
 	},
+}
+
+func getOrg(orgs []count.Msg) (int, count.Msg) {
+	for i, org := range orgs {
+		if orgFilter == org.Organization.Name {
+			return i, org
+		}
+	}
+	return 0, count.Msg{}
+}
+
+type hcVars struct {
+	usageCPU      string
+	usageRAM      string
+	usagePod      string
+	alertsNum     string
+	incidentsNum  string
+	problemsNum   string
+	securitiesNum string
+}
+
+func getInfoWithGoRoutine(auth auth.AuthResponse, clusterId string) hcVars {
+	var res hcVars
+	done := make(chan struct{})
+	go func() {
+		cm, _ := metrics.GetClusterMetrics(auth.AccessToken, clusterId)
+		res.usageCPU = fmt.Sprintf("%.2f%%", cm.CPUUsePercentage)
+		res.usageRAM = fmt.Sprintf("%.2f%%", cm.MemoryUsePercentage)
+		res.usagePod = fmt.Sprintf("%d/%d", cm.TotalPods, cm.TotalPodCapacity)
+		close(done)
+	}()
+	<-done
+
+	done = make(chan struct{})
+	go func() {
+		alerts, _ := alerts.GetAlerts(auth.AccessToken, clusterId)
+		res.alertsNum = fmt.Sprintf("%d", len(alerts))
+		close(done)
+	}()
+	<-done
+
+	done = make(chan struct{})
+	go func() {
+		incidents, _ := incidents.GetIncidents(auth.AccessToken, clusterId)
+		res.incidentsNum = fmt.Sprintf("%d", incidents.Total)
+		close(done)
+	}()
+	<-done
+
+	done = make(chan struct{})
+	go func() {
+		securities, _ := security.GetSecurity(auth.AccessToken, clusterId)
+		res.securitiesNum = fmt.Sprintf("%d", securities.Total)
+		close(done)
+	}()
+	<-done
+
+	done = make(chan struct{})
+	go func() {
+		problems, _ := problem.GetProblems(auth.AccessToken, clusterId)
+		res.problemsNum = fmt.Sprintf("%d", problems.Total)
+		close(done)
+	}()
+	<-done
+
+	return res
 }
---
+++ internal/terminal-utils/tables.go
@@ -0,0 +1,88 @@
+package terminalutils
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type tableModel struct {
+	table table.Model
+}
+
+func (m tableModel) Init() tea.Cmd { return nil }
+
+func (m tableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m tableModel) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
+func RunTables(rows []table.Row) {
+	columns := []table.Column{
+		{Title: "Name", Width: 25},
+		{Title: "ClusterID", Width: 33},
+		{Title: "CPU (%)", Width: 10},
+		{Title: "RAM (%)", Width: 10},
+		{Title: "PODS ", Width: 10},
+		{Title: "Alerts", Width: 10},
+		{Title: "Incidents", Width: 10},
+		{Title: "Security", Width: 10},
+		{Title: "Problems", Width: 10},
+	}
+	fmt.Println(len(rows))
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(len(rows) + 1),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	m := tableModel{t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
